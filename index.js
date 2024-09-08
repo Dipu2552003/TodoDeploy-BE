dd2e5586c920ccf755f03db1fd4c912025e38a7d
dipkul <dipkul.khandelwal21@vit.edu>
2024-09-08 23:39:43 +0530

    ad

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
+require("dotenv").config(); // Ensure this is at the top to load environment variables
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const PORT = 8000;
 const app = express();
-require("dotenv").config();
+
+// MongoDB Connection URI
+const uri = process.env.DATABASE_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Test MongoDB Connection
+async function testMongoDBConnection() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  } finally {
+    await client.close();
+  }
+}
+testMongoDBConnection();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
-// MongoDB Connection
+// MongoDB Connection for Mongoose
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(process.env.DATABASE_URL, {
+  .connect(uri, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     autoIndex: true,
@@ -25,7 +54,7 @@ mongoose
       console.log(`Server is running on port ${PORT}`);
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("Database connection error:", err));
 
 // Routes
 app.get("/", (req, res) => {
@@ -41,6 +70,7 @@ const todoSchema = new mongoose.Schema({
 });
 
 const Todo = mongoose.model("Todo", todoSchema);
+
 // Get all todos
 app.get("/api/todos", async (req, res) => {
   try {
